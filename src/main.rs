d89b920083bc1b33e8b5eccd76d70179a6855153
kuuuube <hexagonisalie@gmail.com>
2024-07-23 01:47:22 -0400

    Basic cli support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use accel_curves::classic::classic;
 use accel_curves::jump::jump;
 use accel_curves::natural::natural;
@@ -11,29 +13,32 @@ mod types;
 mod utility;
 
 fn main() {
-    let mut args = AccelArgs::default();
+    let args: Vec<String> = env::args().collect();
+    let mut accel_args = AccelArgs::default();
     let sens_multiplier = 1.0;
-    args.gain = true;
-    args.cap_mode = CapMode::Output;
+    accel_args.gain = true;
+    accel_args.cap_mode = CapMode::Output;
+
+    let accel_curve = match args.get(1) {
+        Some(some) => some,
+        None => {
+            println!("Invalid args");
+            return;
+        }
+    };
 
     for i in 0..80 {
         let x = i as f64;
-        let noaccel_curve = noaccel(x, &args) * sens_multiplier;
-        let classic_curve = classic(x, &args) * sens_multiplier;
-        let jump_curve = jump(x, &args) * sens_multiplier;
-        let natural_curve = natural(x, &args) * sens_multiplier;
-        let synchronous_curve = synchronous(x, &args) * sens_multiplier;
-        let power_curve = power(x, &args) * sens_multiplier;
-        println!(
-            "{}: {} {} {} {} {} {}",
-            x,
-            noaccel_curve,
-            classic_curve,
-            jump_curve,
-            natural_curve,
-            synchronous_curve,
-            power_curve
-        );
+        let accel = sens_multiplier
+            * match accel_curve.as_str() {
+                "linear" | "classic" => classic(x, &accel_args),
+                "jump" => jump(x, &accel_args),
+                "natural" => natural(x, &accel_args),
+                "synchronous" => synchronous(x, &accel_args),
+                "power" => power(x, &accel_args),
+                _ => noaccel(x, &accel_args),
+            };
+        print!("({},{}),", x, accel);
     }
     print!("\n");
 }
