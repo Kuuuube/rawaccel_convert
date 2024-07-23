c6572e096fe3d1c98ab053a33859dc09507cdd99
kuuuube <hexagonisalie@gmail.com>
2024-07-22 23:39:43 -0400

    Add all curves to main

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
-use accel_curves::classic;
-use types::AccelArgs;
+use accel_curves::classic::classic;
+use accel_curves::jump::jump;
+use accel_curves::natural::natural;
+use accel_curves::noaccel::noaccel;
+use accel_curves::power::power;
+use accel_curves::synchronous::synchronous;
+use types::{AccelArgs, CapMode};
 
 mod accel_curves;
 mod types;
 mod utility;
 
 fn main() {
-    //let mut args = AccelArgs::default();
-    //args.acceleration = 0.0315;
-    //args.exponent_classic = 2.0;
-    //args.cap_mode = types::CapMode::input;
-    //args.cap = [34.0, 0.00001];
-
-    // let sens_multiplier = 1.0;
-    // let accel = classic::classic(80.0, &args) * sens_multiplier;
-    // println!("{}", accel);
-
-    make_points();
-}
-
-fn make_points() {
     let mut args = AccelArgs::default();
     let sens_multiplier = 1.0;
+    args.gain = true;
+    args.cap_mode = CapMode::Output;
 
     for i in 0..80 {
-        let accel = classic::classic(i as f64, &args) * sens_multiplier;
-        print!("({},{}),", i, accel);
+        let x = i as f64;
+        let noaccel_curve = noaccel(x, &args) * sens_multiplier;
+        let classic_curve = classic(x, &args) * sens_multiplier;
+        let jump_curve = jump(x, &args) * sens_multiplier;
+        let natural_curve = natural(x, &args) * sens_multiplier;
+        let synchronous_curve = synchronous(x, &args) * sens_multiplier;
+        let power_curve = power(x, &args) * sens_multiplier;
+        println!(
+            "{}: {} {} {} {} {} {}",
+            x,
+            noaccel_curve,
+            classic_curve,
+            jump_curve,
+            natural_curve,
+            synchronous_curve,
+            power_curve
+        );
     }
     print!("\n");
 }
