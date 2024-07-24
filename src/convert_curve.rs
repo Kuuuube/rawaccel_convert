d2ef5d7f9034b42924e72f29a82354a65947d326
kuuuube <hexagonisalie@gmail.com>
2024-07-24 13:35:41 -0400

    Support single point sens to velocity conversion

--- src/convert_curve.rs
+++ src/convert_curve.rs
@@ -3,10 +3,11 @@ use crate::types::Point;
 pub fn sensitivity_to_velocity(sensitivity_accel_curve: Vec<Point>) -> Vec<Point> {
     let mut new_accel_curve: Vec<Point> = vec![];
     for sensitivity_point in sensitivity_accel_curve {
-        new_accel_curve.push(Point {
-            x: sensitivity_point.x,
-            y: sensitivity_point.y * sensitivity_point.x,
-        })
+        new_accel_curve.push(sensitivity_point_to_velocity(sensitivity_point))
     }
     return new_accel_curve;
+}
+
+pub fn sensitivity_point_to_velocity(sensitivity_point: Point) -> Point {
+    return Point { x: sensitivity_point.x, y: sensitivity_point.x * sensitivity_point.y };
 }
