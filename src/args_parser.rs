7ca5a620683463c49af3fe496739fb1acbf3e29d
kuuuube <hexagonisalie@gmail.com>
2024-07-25 00:47:26 -0400

    Aggressively strip whitespace from lookup table data

--- src/args_parser.rs
+++ src/args_parser.rs
@@ -364,12 +364,13 @@ pub fn parser(args: Vec<String>) -> Option<AccelArgs> {
 
 pub fn parse_lookup_table(input_string: &str) -> Option<Vec<Vec2>> {
     let mut parsed_points = vec![];
-    let points: Vec<&str> = input_string.trim().split(";").collect();
+    let newlines_stripped = input_string.replace("\n", "").replace(" ", "");
+    let points: Vec<&str> = newlines_stripped.trim().split(";").collect();
     for point in points {
         let xy: Vec<&str> = point.split(",").collect();
         if xy.len() == 2 {
-            let x_parsed = unwrap_result_or_continue!(xy[0].parse::<f64>());
-            let y_parsed = unwrap_result_or_continue!(xy[1].parse::<f64>());
+            let x_parsed = unwrap_result_or_continue!(xy[0].trim().parse::<f64>());
+            let y_parsed = unwrap_result_or_continue!(xy[1].trim().parse::<f64>());
             parsed_points.push(Vec2 {
                 x: x_parsed,
                 y: y_parsed,
